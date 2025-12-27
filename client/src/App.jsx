8cda604020ab2b150abd7430980709ec117350d9
hong2354 <139599735+hong2354@users.noreply.github.com>
2025-12-27 17:04:49 +0800

    项目创建

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>实时协作文本编辑器</h1>
+      <p>欢迎来到我的毕业设计项目！</p>
+    </div>
+  )
+}
+
+export default App
